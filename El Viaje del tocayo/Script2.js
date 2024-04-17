5bb0216177009a34506980b0461539963b706162
Julian G <juliancamilo0210@gmail.com>
2024-04-17 16:46:15 -0500

    Add the challenge two finished

--- El Viaje del tocayo/Script2.js
+++ El Viaje del tocayo/Script2.js
@@ -10,10 +10,10 @@ function ingresarSouvenirs(){
     alert("Ahora vamos a crear souvenirs")
 
     souvenirs.name = prompt("Ingrese el nombre del souvenir")
-    souvenirs.price = Number(prompt("Ingrese el precio del souvenir"))
+    souvenirs.price = parseFloat(prompt("Ingrese el precio del souvenir"))
     souvenirs.availability = confirm("¿Esta disponible el souvenir?")
 
-    verificadorDatos(souvenirs)
+    verificadorDatos()
 
     listSouvenirs.push(souvenirs)
 
@@ -23,20 +23,32 @@ function ingresarSouvenirs(){
     }
 }
 
-function verificadorDatos(objeto){
+function verificadorDatos(){
     let text = " "
     let num = 1.0
     let bool = true
 
-    if ( typeof(objeto.name)===typeof(text) && typeof(objeto.price)==typeof(num) && typeof(objeto.availability)===typeof(bool)){
+     let recipiente = souvenirs.price
+
+    if(isNaN(recipiente)){
+        alert("El objeto no es valido")
+        restSouvenirs()
+    }
+    else if ( typeof(souvenirs.name)===typeof(text) && typeof(souvenirs.price)==typeof(num) && typeof(souvenirs.availability)===typeof(bool)){
         alert("El objeto fue creado correctamente")
     }
     else{
         alert("El objeto no es valido")
-        // resetSouvenirs(souvenirs)
+        restSouvenirs()
     }
 }
 
-// resetSouvenirs()
+function restSouvenirs (){
+    souvenirs.name = undefined
+    souvenirs.price= undefined
+    souvenirs.availability = undefined
+    return ingresarSouvenirs()
+}
 
 ingresarSouvenirs()
+console.log(listSouvenirs)
